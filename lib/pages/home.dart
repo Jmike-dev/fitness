c5d17be7d8b365c8f12ebb697fe4ab7efcb18d66
Michael Gichuhi <gmikekamau@gmail.com>
2023-08-10 15:38:42 +0300

    🚧 learned how to add fonts and icons to flutter app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,35 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
- class Homepage extends StatelessWidget {
+class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text(
+          'breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          child: SvgPicture.asset(
+            'assets/icons/left-arrow.svg',
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
   }
 }
